7d9008bb080626eb6f37db3d3f8499ca90738e6d
GautamRaj-1200 <gautamrajv2@gmail.com>
2025-08-26 17:27:41 +0530

    Add home page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
 
   return (
     <section className="mx-auto max-w-5xl py-8">
-      <h1 className="font-sour-gummy text-center text-xl font-bold text-[var(--accent-color)]">
+      <h1 className="font-sour-gummy mb-8 px-4 text-center text-xl font-bold text-[var(--accent-color)]">
         What follows are my learnings, thoughts, and personal perspective.
       </h1>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 px-4">
         {posts.map((post) => (
           <PostCard key={post.id} post={post} />
         ))}
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -4,8 +4,18 @@ import Link from "next/link";
 const PostCard = ({ post }: { post: Post }) => {
   return (
     <>
-      <Link href={`/posts/${post.id}`}>
-        <h2 className="text-lg font-bold">{post.title}</h2>
+      <Link
+        href={`/posts/${post.id}`}
+        className="flex items-center justify-between rounded-lg bg-[#0c1320] p-2 transition-colors duration-300 ease-in-out hover:bg-[#1a2330]"
+      >
+        <h2 className="font-sour-gummy text-accent-color text-lg font-bold">
+          {post.title}
+        </h2>
+        <p className="font-sour-gummy text-sm text-gray-500">
+          {post.createdAt instanceof Date
+            ? post.createdAt.toLocaleDateString()
+            : new Date(post.createdAt).toLocaleDateString()}
+        </p>
       </Link>
     </>
   );
